42a538b2204a828273842df00c3c0ab09d17bc9f
Ethan Pryce <ethan.m.d.pryce@gmail.com>
2021-06-22 17:07:11 -0700

    Added the assignment

---
+++ src/pages/Home.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import Posts from '../components/Posts';
+
+function Home() {
+  const [appState, setAppState] = useState({
+    loading: false,
+    posts: null,
+  });
+
+  useEffect( () => {
+    setAppState({ loading: true });
+    const apiUrl = "https://jsonplaceholder.typicode.com/posts";
+    axios.get(apiUrl).then((postList) => {
+      const allPosts = postList.data;
+      const topPosts = allPosts.slice(0,5);
+      setAppState({ loading: false, posts: topPosts} );
+    });
+
+    
+  }, [setAppState]);
+
+  return (
+      <Posts isLoading={appState.loading} posts={appState.posts}/>
+  );
+
+}
+export default Home;
